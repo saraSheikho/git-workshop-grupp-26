2e7f2d11939819cb36549422e4339d74fba298bd
Sara <sarasheikho885@gmail.com>
2025-03-28 10:46:12 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String name = input.nextLine();
+        if(name.equals("Sara")){
+            Sara sara = new Sara();
+            sara.sayHello("Hello from Sara");
+        }else if (name.equals("Lilas")){
+            Lilas lilas = new Lilas();
+            lilas.sayHello("Hello from Lilas");
+        }else{
+            Ahmad ahmad = new Ahmad();
+            ahmad.sayHello("Hello from Ahmad");
+        }
     }
 }
