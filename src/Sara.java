a697233a664596bc671629da7116803cedd9efdf
Sara <sarasheikho885@gmail.com>
2025-03-28 10:59:07 +0100

    Update Sara.java

--- src/Sara.java
+++ src/Sara.java
@@ -2,4 +2,7 @@ public class Sara {
     public void sayHello(String message){
         System.out.println(message);
     }
+    public void food(){
+        System.out.println("Sara gillar citron");
+    }
 }
