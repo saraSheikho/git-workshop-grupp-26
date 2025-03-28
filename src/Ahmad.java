15992d21848fa2f96b575e99b562ee23e3b2b414
ahmadrules <ahmattomaar@gmail.com>
2025-03-28 10:58:38 +0100

    Update Ahmad.java

--- src/Ahmad.java
+++ src/Ahmad.java
@@ -2,4 +2,8 @@ public class Ahmad {
     public void sayHello(String message) {
         System.out.println(message);
     }
+
+    public void food(){
+        System.out.println("Pasta");
+    }
 }
